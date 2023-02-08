a3170e62f5284740930d8228cf5caaa7f1ba7459
rskiver89 <108497515+rskiver89@users.noreply.github.com>
2023-02-08 15:48:44 -0500

    popular movie layout fix

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,14 +1,16 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState, useContext} from 'react'
 import '../styles/Home.css'
 import Slider from '../components/Slider'
 import axios from 'axios'
 import MovieCard from '../components/MovieCard'
+import { ThemeContext } from '../context/ThemeContext'
 
 function Homepage({apiKey, baseUrl}) {
   const [popularMovies, setPopularMovies]=useState([])
   const [topRatedMovies, setTopRatedMovies]=useState([])
   const pageNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   const [page, setPage]=useState([1])
+  const {darkMode, setDarkMode}=useContext(ThemeContext);
 
   useEffect(() => {
     axios.get(`${baseUrl}/movie/popular?api_key=${apiKey}&page=${page}`)
@@ -22,43 +24,44 @@ function Homepage({apiKey, baseUrl}) {
   useEffect(() => {
     axios.get(`${baseUrl}/movie/top_rated?api_key=${apiKey}&page=${page}`)
     .then(res=>{
-      setTopRatedMovies(res.data.results)
+      setTopRatedMovies(res.data.results.slice(0,10))
     })
     .catch(err=> console.log(err))
   }, [])
 
   return (
-    <div className="homepage-container">
+    <div className={darkMode ? "homepage-container" : "homepage-container header-light"}>
       <Slider apiKey={apiKey} baseUrl={baseUrl}/>
       <div className="movies-wrapper">
         <div className="popular-container">
-        <h3 className="popular-title">Popular</h3>
-          <div className="popular-cards-wrapper">
-        {
-          popularMovies?.map(movie => {
-            return <MovieCard height="300px" imageUrl={movie.poster_path} cardStyle={"popular-card"} data={movie} radius={"16px"}/>
-          })
-        }
-          </div>
-        </div>
-
-        <div className="page-numbers">
-          <p>Select Page</p>
-        {
-          pageNumbers.map(item=>{
-            return <p onClick={()=>setPage(item)}>{item}</p>
-          })
-        }
-        </div>
-          <div className="top-rated-container">
-            <div className="top-rated-cards-wrapper">
-            <h3>Top Rated</h3>
+          <h3 className="popular-title"></h3>
+            <div className="popular-cards-wrapper">
           {
-          topRatedMovies?.map(movie => {
-            return <MovieCard height="100px" imageUrl={movie.backdrop_path} cardStyle={"top-rated-card"} data={movie} radius={"8px"}/>
-          })
-        }
-          </div>
+            popularMovies?.map(movie => {
+              return <MovieCard height="300px" imageUrl={movie.poster_path} cardStyle={"popular-card"} data={movie} radius="16px"/>
+            })
+          }
+            </div>
+
+            <div className="page-numbers">
+              <p>Select Page</p>
+            {
+              pageNumbers.map(item=>{
+                return <p onClick={()=>setPage(item)}>{item}</p>
+              })
+            }
+            </div>
+
+        </div>
+              <div className="top-rated-container">
+                <div className="top-rated-cards-wrapper">
+                <h3>Top Rated</h3>
+              {
+              topRatedMovies?.map(movie => {
+                return <MovieCard height="100px" imageUrl={movie.backdrop_path} cardStyle={"top-rated-card"} data={movie} radius={"8px"}/>
+              })
+            }
+              </div>
           </div>
       </div>
     </div>
